3b5531f8cb34d75412cc4db86b6617bb5157da47
johnn <johnny_kl@126.com>
2019-04-28 13:58:24 +0800

    test

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/Application.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/Application.java
@@ -10,10 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("org.linlinjava.litemall.db.dao")
 @EnableTransactionManagement
 @EnableScheduling
-
-
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
