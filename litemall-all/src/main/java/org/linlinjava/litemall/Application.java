d7ea55c8dfd99998beddb2aa4c9468e101d7275c
johnn <johnny_kl@126.com>
2019-05-22 19:24:00 +0800

    加入https,jdk生成的ssl证书

--- litemall-all/src/main/java/org/linlinjava/litemall/Application.java
+++ litemall-all/src/main/java/org/linlinjava/litemall/Application.java
@@ -1,8 +1,15 @@
 package org.linlinjava.litemall;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,5 +22,26 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
+    //下面是2.0的配置，1.x请搜索对应的设置
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（8388）、https（8443）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+
+        connector.setPort(8388);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+
 
 }
