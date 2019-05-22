cbfb79a7a98f07d5b2134a55b9b55b81bcc0a32b
johnn <johnny_kl@126.com>
2019-05-22 19:28:15 +0800

    修改后台访问路径，https和http

--- litemall-admin/config/dev.env.js
+++ litemall-admin/config/dev.env.js
@@ -1,5 +1,7 @@
 module.exports = {
 	NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://localhost:8080/admin"'
+  // BASE_API: '"http://www.cduhw.cn:8388/admin"'
+  //BASE_API: '"http://localhost:8388/admin"'
+  BASE_API: '"https://localhost:8443/admin"'
 }
--- litemall-admin/config/prod.env.js
+++ litemall-admin/config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-    BASE_API: '"https://www.example.com/admin"'
+	// BASE_API: '"https://www.example.com/admin"'
+	BASE_API:'"http://www.cduhw.cn:8388/admin"'
 }
